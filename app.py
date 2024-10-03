fb811e3e79f0dd47f7b1bb44b47f84d508a1ad02
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-10-04 03:26:38 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -597,4 +597,7 @@ def add_flower(name):
 @app.route("/lab2/example")
 def example():
     name = 'Анжелика Оноприенко'
-    return render_template('example.html', name=name)
+    number = '2'
+    group = '21'
+    course = '3'
+    return render_template('example.html', name=name, number=number, group=group, course=course)
