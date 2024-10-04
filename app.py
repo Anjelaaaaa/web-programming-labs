e39e4055bebdee6659f26de3eb29b347dd716a61
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-10-04 15:24:47 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -600,4 +600,11 @@ def example():
     number = '2'
     group = 'ФБИ-21'
     course = '3 курс'
-    return render_template('example.html', number=number, name=name, group=group, course=course)
+    fruits = [
+        {'name':'яблоки', 'price': 100},
+        {'name':'груши', 'price': 120},
+        {'name':'апельсины', 'price': 80},
+        {'name':'мандарины', 'price': 95},
+        {'name':'манго', 'price': 321}
+    ]
+    return render_template('example.html', number=number, name=name, group=group, course=course, fruits=fruits)
