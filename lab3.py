8dc5735d078a65a0666737c045ef66faba7ee355
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-10-23 04:50:22 +0700

    Игры

--- lab3.py
+++ lab3.py
@@ -110,7 +110,7 @@ def del_settings():
 def ticket():
     ticket = 0
     FIO = request.args.get('FIO')
-    age = request.args.get('age')
+    age = request.args.get('age', 0)
     shelf = request.args.get('shelf')
     linen = request.args.get('linen')
     baggage = request.args.get('baggage')
@@ -118,11 +118,7 @@ def ticket():
     place2 = request.args.get('place2')
     date = request.args.get('date')
     belay = request.args.get('belay')
-    if age is None:
-        age = 0
-    else:
-        age = int(age)
-    if age < 18:
+    if int(age) < 18:
         ticket += 700
         ticket_type = "Детский билет"
     else:
@@ -137,4 +133,46 @@ def ticket():
     if belay == 'withbelay':
         ticket += 150
     return render_template('lab3/ticket.html', FIO=FIO, age=age, ticket=ticket, shelf=shelf, linen=linen, baggage=baggage, belay=belay, ticket_type=ticket_type, place1=place1, place2=place2, date=date)
+
+
+games = [
+    {'name': 'Cyberpunk 2077', 'price': 2999, 'genre': 'RPG', 'weight': 57},
+    {'name': 'The Witcher 3: Wild Hunt', 'price': 599, 'genre': 'RPG', 'weight': 32},
+    {'name': 'Grand Theft Auto V', 'price': 2199, 'genre': 'Action', 'weight': 85},
+    {'name': 'Red Dead Redemption 2', 'price': 2493, 'genre': 'Action', 'weight': 150},
+    {'name': 'Elden Ring', 'price': 3899, 'genre': 'RPG', 'weight': 47},
+    {'name': 'God of War', 'price': 1968, 'genre': 'Action', 'weight': 118},
+    {'name': 'Horizon Zero Dawn', 'price': 1999, 'genre': 'Action', 'weight': 67},
+    {'name': 'Ghost of Tsushima', 'price': 1549, 'genre': 'Action', 'weight': 46},
+    {'name': 'Marvels Spider-Man', 'price': 2550, 'genre': 'Action', 'weight': 54},
+    {'name': 'Death Stranding', 'price': 1299, 'genre': 'Action', 'weight': 35},
+    {'name': 'The Last of Us Part II', 'price': 3499, 'genre': 'Action', 'weight': 82},
+    {'name': 'Uncharted 4: A Thiefs End', 'price': 2200, 'genre': 'Action', 'weight': 32},
+    {'name': 'Resident Evil Village', 'price': 4353, 'genre': 'Horror', 'weight': 91},
+    {'name': 'Doom Eternal', 'price': 999, 'genre': 'Shooter', 'weight': 28},
+    {'name': 'Hades', 'price': 799, 'genre': 'Roguelike', 'weight': 23},
+    {'name': 'Stardew Valley', 'price': 650, 'genre': 'Simulation', 'weight': 19},
+    {'name': 'Terraria', 'price': 575, 'genre': 'Sandbox', 'weight': 15},
+    {'name': 'Minecraft', 'price': 500, 'genre': 'Sandbox', 'weight': 24},
+    {'name': 'Among Us', 'price': 499, 'genre': 'Multiplayer', 'weight': 10},
+    {'name': 'Rocket League', 'price': 876, 'genre': 'Sports', 'weight': 16}
+]
+
+@lab3.route('/lab3/games')
+def games_search():
+    min_price = request.args.get('min_price')
+    max_price = request.args.get('max_price')
+
+    if min_price is None or max_price is None:
+        return render_template('lab3/games.html', filtered_games=games, min_price=min_price, max_price=max_price)
+    
+    min_price = int(min_price)
+    max_price = int(max_price)
+
+    if max_price < min_price:
+        return render_template('lab3/games.html', error="Максимальная цена не может быть меньше минимальной", min_price=min_price, max_price=max_price)
+    
+    filtered_games = [game for game in games if min_price <= game['price'] <= max_price]
+
+    return render_template('lab3/games.html', filtered_games=filtered_games, min_price=min_price, max_price=max_price)
 
