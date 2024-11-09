3ae35471978c3122677f85bc648e86e4f17530b7
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-11-09 17:40:44 +0700

    Улучшения

--- lab5.py
+++ lab5.py
@@ -1,7 +1,24 @@
 from flask import Blueprint, render_template, request, redirect, session
-lab5 = Blueprint('lab5', __name__)
 import psycopg2
 from psycopg2.extras import RealDictCursor
+lab5 = Blueprint('lab5', __name__)
+
+
+def db_connect():
+    conn = psycopg2.connect (
+        host = '127.0.0.1',
+        database = 'anjelika_onoprienko_knowledge_base',
+        user = 'anjelika_onoprienko_knowledge_base',
+        password = '893920'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 @lab5.route('/lab5/')
@@ -12,64 +29,47 @@ def lab():
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return render_template('lab5/register.html')
+        return render_template('lab5/register.html', login=session.get('login'))
     login = request.form.get('login')
     password = request.form.get('password')
     if not login or not password:
         return render_template('lab5/register.html', error='Заполните все поля')
         
-    conn = psycopg2.connect (
-        host = '127.0.0.1',
-        database = 'anjelika_onoprienko_knowledge_base',
-        user = 'anjelika_onoprienko_knowledge_base',
-        password = '893920'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html', error='Такой пользователь уже существует')
 
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}');") 
-    conn.commit()
-    cur.close()
-    conn.close()
+
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 
 @lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('lab5/login.html')
+        return render_template('lab5/login.html', login=session.get('login'))
     login = request.form.get('login')
     password = request.form.get('password')
     if not login or not password:
         return render_template('lab5/login.html', error='Заполните все поля')
 
-    conn = psycopg2.connect (
-        host = '127.0.0.1',
-        database = 'anjelika_onoprienko_knowledge_base',
-        user = 'anjelika_onoprienko_knowledge_base',
-        password = '893920'
-    )
-    cur = conn.cursor(cursor_factory = RealDictCursor)
+    conn, cur = db_connect()
 
     cur.execute(f"SELECT * FROM users WHERE login='{login}';")
     user = cur.fetchone()
 
     if not user:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
     if user['password'] != password:
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/login.html', error='Логин и/или пароль неверны')
     
     session['login'] = login
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success_login.html', login=login)
