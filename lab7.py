cbcefda859a9b94e7e965a9145143238198a7108
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-12-12 00:33:45 +0700

    База данных

--- lab7.py
+++ lab7.py
@@ -1,7 +1,35 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
 from datetime import datetime
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import sqlite3
+from os import path
 
 lab7 = Blueprint('lab7', __name__)
+
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='anjelika_onoprienko_knowledge_base',
+            user='anjelika_onoprienko_knowledge_base',
+            password='893920'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 @lab7.route('/lab7/')
@@ -9,143 +37,135 @@ def lab():
     return render_template('lab7/lab7.html')
 
 
-films = [
-    {
-        "title_ru": "Никто",
-        "title": "Nobody",
-        "year": 2021,
-        "description": "Непримечательный и незаметный семьянин \
-            Хатч живёт скучной жизнью обычного аудитора, пока \
-            однажды в его дом не вламываются грабители. И это \
-            бы сошло им с рук, если бы они не забрали браслетик \
-            его маленькой дочки. Не в силах это терпеть, Хатч \
-            отправляется на поиски наглецов, а на обратном пути \
-            ввязывается в драку с пьяными хулиганами, \
-            пристававшими к девушке в общественном транспорте. \
-            От души помахав кулаками, наш аудитор отправляет \
-            дебоширов в больницу, но оказывается, что один из \
-            пострадавших — брат влиятельного русского бандита. \
-            И он теперь жаждет мести."
-    },
-    {
-        "title_ru": "Шерлок",
-        "title": "Sherlock",
-        "year": 2010,
-        "description": "События разворачиваются в наши дни. \
-            Он прошел Афганистан, остался инвалидом. По \
-            возвращении в родные края встречается с загадочным, \
-            но своеобразным гениальным человеком. Тот в поиске \
-            соседа по квартире. Лондон, 2010 год. Происходят \
-            необъяснимые убийства. Скотланд-Ярд без понятия, \
-            за что хвататься. Существует лишь один человек, \
-            который в силах разрешить проблемы и найти ответы \
-            на сложные вопросы."
-    },
-    {
-        "title_ru": "Зеленая книга",
-        "title": "Green Book",
-        "year": 2018,
-        "description": "1960-е годы. После закрытия нью-йоркского \
-            ночного клуба на ремонт вышибала Тони по прозвищу Болтун \
-            ищет подработку на пару месяцев. Как раз в это время \
-            Дон Ширли — утонченный светский лев, богатый и \
-            талантливый чернокожий музыкант, исполняющий классическую \
-            музыку — собирается в турне по южным штатам, где ещё \
-            сильны расистские убеждения и царит сегрегация. Он \
-            нанимает Тони в качестве водителя, телохранителя и \
-            человека, способного решать текущие проблемы. У этих \
-            двоих так мало общего, и эта поездка навсегда изменит \
-            жизнь обоих."
-    },
-    {
-        "title_ru": "1+1",
-        "title": "Intouchables",
-        "year": 2011,
-        "description": "Пострадав в результате несчастного случая, \
-            богатый аристократ Филипп нанимает в помощники человека, \
-            который менее всего подходит для этой работы, – молодого \
-            жителя предместья Дрисса, только что освободившегося \
-            из тюрьмы. Несмотря на то, что Филипп прикован к \
-            инвалидному креслу, Дриссу удается привнести в \
-            размеренную жизнь аристократа дух приключений."
-    },
-    {
-        "title_ru": "Благие знамения",
-        "title": "Good Omens",
-        "year": 2019,
-        "description": "Ангел Азирафель и демон Кроули несколько \
-            тысяч лет живут среди людей и за это время успели \
-            подружиться, хотя это и противоречит строгим правилам \
-            небесной и адской канцелярий. Узнав о предстоящем \
-            конце света, они решают объединить усилия, чтобы \
-            предотвратить апокалипсис, поскольку уже успели \
-            привыкнуть к жизни на Земле."
-    },
-]
-
-
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM films;")
+    else:
+        cur.execute("SELECT * FROM films;")
+
+    films = cur.fetchall()
+    db_close(conn, cur)
+
     return films
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['GET'])
 def get_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("SELECT * FROM films WHERE id = ?;", (id,))
+
+    film = cur.fetchone()
+    db_close(conn, cur)
+
+    if film is None:
         return {"error": "Film not found"}, 404
-    return films[id]
+    
+    return film
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
 def del_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("DELETE FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("DELETE FROM films WHERE id = ?;", (id,))
+
+    db_close(conn, cur)
+
+    if film is None:
         return {"error": "Film not found"}, 404
-    del films[id]
     return '', 204
 
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
 def put_film(id):
-    if id < 0 or id >= len(films):
+    conn, cur = db_connect()
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM films WHERE id = %s;", (id,))
+    else:
+        cur.execute("SELECT * FROM films WHERE id = ?;", (id,))
+    
+    film_in_db = cur.fetchone()
+
+    if film_in_db is None:
+        db_close(conn, cur)
         return {"error": "Film not found"}, 404
+
     film = request.get_json()
-    if film['title_ru'] == '':
+
+    if film.get('title_ru') == '':
         return {'title_ru': 'Укажите русское название'}, 400
-    if film['year'] == '':
+    if film.get('year') == '':
         return {'year': 'Укажите год'}, 400
+
     current_year = datetime.now().year
     year = int(film['year'])
     if year < 1895 or year > current_year:
         return {'year': f'Год должен быть от 1895 до {current_year}'}, 400
-    if film['description'] == '':
+    
+    if film.get('description') == '':
         return {'description': 'Заполните описание'}, 400
-    if film['title'] == '':
-        film['title'] = film['title_ru']
-    films[id] = film
-    return films[id]
+
+    if len(film.get('description', '')) > 2000:
+        return {'description': 'Описание превышает 2000 символов'}, 400
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("UPDATE films SET title = %s, title_ru = %s, year = %s, description = %s WHERE id = %s;", 
+                    (film['title'], film['title_ru'], film['year'], film['description'], id))
+    else:
+        cur.execute("UPDATE films SET title = ?, title_ru = ?, year = ?, description = ? WHERE id = ?;", 
+                    (film['title'], film['title_ru'], film['year'], film['description'], id))
+    
+    db_close(conn, cur)
+    return film
+
+
 
 
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
+    conn, cur = db_connect()
     film = request.get_json()
-    if film['title'] == '' and film['title_ru'] == '':
+
+    if film.get('title') == '' and film.get('title_ru') == '':
         return {'title': 'Укажите оригинальное название'}, 400
-    if film['title_ru'] == '':
+    if film.get('title_ru') == '':
         return {'title_ru': 'Укажите русское название'}, 400
-    if film['year'] == '':
+    if film.get('year') == '':
         return {'year': 'Укажите год'}, 400
+
     current_year = datetime.now().year
     year = int(film['year'])
     if year < 1895 or year > current_year:
         return {'year': f'Год должен быть от 1895 до {current_year}'}, 400
-    if film['description'] == '':
-        return {'description': 'Заполните описание'}, 400
-    description = film.get('description', '')
-    if len(description) > 2000:
+    
+    if len(film.get('description', '')) > 2000:
         return {'description': 'Описание превышает 2000 символов'}, 400
-    if film['title'] == '':
-        film['title'] = film['title_ru']
-    films.append(film)
-    id = {'id': len(films) - 1}
-    return id
+
+    if film.get('description') == '':
+        return {'description': 'Заполните описание'}, 400
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("INSERT INTO films (title, title_ru, year, description) VALUES (%s, %s, %s, %s) RETURNING id;", 
+                    (film['title'], film['title_ru'], film['year'], film['description']))
+        film_id = cur.fetchone()['id']
+    else:
+        cur.execute("INSERT INTO films (title, title_ru, year, description) VALUES (?, ?, ?, ?);", 
+                    (film['title'], film['title_ru'], film['year'], film['description']))
+        film_id = cur.lastrowid
+
+    db_close(conn, cur)
+    
+    return {'id': film_id}
+
 
--- static/lab7/main.js
+++ static/lab7/main.js
@@ -1,68 +1,68 @@
 function fillFilmList() {
     fetch('/lab7/rest-api/films/')
-    .then(function (data) {
-        return data.json();
-    })
-    .then(function (films) {
-        let tbody = document.getElementById('film-list');
-        tbody.innerHTML = '';
-        for(let i = 0; i<films.length; i++) {
-            let tr = document.createElement('tr');
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (films) {
+            let tbody = document.getElementById('film-list');
+            tbody.innerHTML = '';
+            for (let i = 0; i < films.length; i++) {
+                let tr = document.createElement('tr');
 
-            let tdTitleRus = document.createElement('td');
-            let tdTitle = document.createElement('td');
-            let tdYear = document.createElement('td');
-            let tdAction = document.createElement('td');
+                let tdTitleRus = document.createElement('td');
+                let tdTitle = document.createElement('td');
+                let tdYear = document.createElement('td');
+                let tdAction = document.createElement('td');
 
-            tdTitleRus.innerText = films[i].title_ru;
-            let originalTitle = document.createElement('span');
-            originalTitle.innerText = films[i].title === films[i].title_ru ? '' : films[i].title;
-            originalTitle.style.fontStyle = 'italic';
-            originalTitle.innerText = originalTitle.innerText ? ` (${originalTitle.innerText})` : '';
-            tdTitle.appendChild(originalTitle);
-            tdYear.innerText = films[i].year;
+                tdTitleRus.innerText = films[i].title_ru;
+                let originalTitle = document.createElement('span');
+                originalTitle.innerText = films[i].title === films[i].title_ru ? '' : films[i].title;
+                originalTitle.style.fontStyle = 'italic';
+                originalTitle.innerText = originalTitle.innerText ? ` (${originalTitle.innerText})` : '';
+                tdTitle.appendChild(originalTitle);
+                tdYear.innerText = films[i].year;
 
-            let editButton = document.createElement('button');
-            editButton.innerText = 'Редактировать';
-            editButton.style.backgroundColor = 'rgb(217, 228, 248)';
-            editButton.style.border = '1px';
-            editButton.style.padding = '7px';
-            editButton.style.margin = '5px';
-            editButton.style.borderRadius = '7px';
+                let editButton = document.createElement('button');
+                editButton.innerText = 'Редактировать';
+                editButton.style.backgroundColor = 'rgb(217, 228, 248)';
+                editButton.style.border = '1px';
+                editButton.style.padding = '7px';
+                editButton.style.margin = '5px';
+                editButton.style.borderRadius = '7px';
 
-            editButton.onclick = function() {
-                editFilm(i);
+                editButton.onclick = function () {
+                    editFilm(films[i].id);
+                }
+
+                let delButton = document.createElement('button');
+                delButton.innerText = 'Удалить';
+                delButton.style.border = '2px solid rgb(217, 228, 248)';
+                delButton.style.padding = '6px';
+                delButton.style.margin = '5px';
+                delButton.style.borderRadius = '7px';
+
+                delButton.onclick = function () {
+                    deleteFilm(films[i].id, films[i].title_ru);
+                }
+
+                tdAction.append(editButton);
+                tdAction.append(delButton);
+
+                tr.append(tdTitleRus);
+                tr.append(tdTitle);
+                tr.append(tdYear);
+                tr.append(tdAction);
+
+                tbody.append(tr);
             }
-
-            let delButton = document.createElement('button');
-            delButton.innerText = 'Удалить';
-            delButton.style.border = '2px solid rgb(217, 228, 248)';
-            delButton.style.padding = '6px';
-            delButton.style.margin = '5px';
-            delButton.style.borderRadius = '7px';
-
-            delButton.onclick = function() {
-                deleteFilm(i, films[i].title_ru);
-            }
-
-            tdAction.append(editButton);
-            tdAction.append(delButton);
-
-            tr.append(tdTitleRus);
-            tr.append(tdTitle);
-            tr.append(tdYear);
-            tr.append(tdAction);
-
-            tbody.append(tr);
-        }
-    })
+        })
 }
 
 function deleteFilm(id, title) {
-    if(! confirm(`Вы точно хотите удалить фильм "${title}"?`))
+    if (!confirm(`Вы точно хотите удалить фильм "${title}"?`))
         return;
 
-    fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
+    fetch(`/lab7/rest-api/films/${id}`, { method: 'DELETE' })
         .then(function () {
             fillFilmList();
         });
@@ -77,42 +77,42 @@ function sendFilm() {
         description: document.getElementById('description').value
     }
 
-    const url = `/lab7/rest-api/films/${id}`;
-    const method = id === '' ? 'POST' : 'PUT';
+    const url = id ? `/lab7/rest-api/films/${id}` : '/lab7/rest-api/films/';
+    const method = id ? 'PUT' : 'POST';
 
     fetch(url, {
         method: method,
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(film)
     })
-    .then(function(resp) {
-        if(resp.ok) {
-            fillFilmList();
-            hideModal();
-        }
-        return resp.json();
-    })
-    .then(function(errors) {
-        document.getElementById('year-error').innerText = errors.year || '';
-        document.getElementById('description-error').innerText = errors.description || '';
-        document.getElementById('titleru-error').innerText = errors.title_ru || '';
-        document.getElementById('title-error').innerText = errors.title || '';
-    });
+        .then(function (resp) {
+            if (resp.ok) {
+                fillFilmList();
+                hideModal();
+            }
+            return resp.json();
+        })
+        .then(function (errors) {
+            document.getElementById('year-error').innerText = errors.year || '';
+            document.getElementById('description-error').innerText = errors.description || '';
+            document.getElementById('titleru-error').innerText = errors.title_ru || '';
+            document.getElementById('title-error').innerText = errors.title || '';
+        });
 }
 
 function editFilm(id) {
     fetch(`/lab7/rest-api/films/${id}`)
-    .then(function (data) {
-        return data.json();
-    })
-    .then(function (film) {
-        document.getElementById('id').value = id;
-        document.getElementById('title').value = film.title;
-        document.getElementById('title-ru').value = film.title_ru;
-        document.getElementById('year').value = film.year;
-        document.getElementById('description').value = film.description;
-        showModal();
-    });
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (film) {
+            document.getElementById('id').value = film.id;
+            document.getElementById('title').value = film.title;
+            document.getElementById('title-ru').value = film.title_ru;
+            document.getElementById('year').value = film.year;
+            document.getElementById('description').value = film.description;
+            showModal();
+        });
 }
 
 function showModal() {
