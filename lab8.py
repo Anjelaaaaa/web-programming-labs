7cc07042b65e875f0cc5a26c6feccbf0a8462c59
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-12-15 03:38:51 +0700

    Автоматический логин

--- lab8.py
+++ lab8.py
@@ -9,11 +9,16 @@ lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
 def lab():
+    if current_user.is_authenticated:
+        return render_template('lab8/lab8.html', login=current_user.login)
     return render_template('lab8/lab8.html')
 
 
 @lab8.route('/lab8/register', methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return render_template('lab8/register.html', login=current_user.login, message='Вы не можете зарегистрироваться, находясь в аккаунте. Хотите выйти?')
+    
     if request.method == 'GET':
         return render_template('lab8/register.html')
 
@@ -34,11 +39,15 @@ def register():
     new_user = users(login = login_form, password = password_hash)
     db.session.add(new_user)
     db.session.commit()
-    return render_template('lab8/success.html', login=login_form)
+    login_user(new_user)
+    return render_template('lab8/success_login.html', login=login_form)
 
 
 @lab8.route('/lab8/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return render_template('lab8/success_login.html', login=current_user.login)
+        
     if request.method == 'GET':
         return render_template('lab8/login.html')
 
@@ -52,9 +61,11 @@ def login():
         return render_template('lab8/login.html', error='Заполните пароль')
 
     user = users.query.filter_by(login = login_form).first()
+    remember = request.form.get('remember', 'False') == 'True'
+    
     if user:
         if check_password_hash(user.password, password_form):
-            login_user(user, remember = False)
+            login_user(user, remember=remember)
             return render_template('lab8/success_login.html', login=login_form)
         
     return render_template('lab8/login.html', error='Ошибка входа: логин и/или пароль неверны')
@@ -63,7 +74,8 @@ def login():
 @lab8.route('/lab8/articles/')
 @login_required
 def article_list():
-    return "список статей"
+    articles_list = articles.query.filter_by(login_id=current_user.id).all()
+    return render_template('lab8/articles.html', articles=articles_list, login=current_user.login)
 
 
 @lab8.route('/lab8/logout')
@@ -71,4 +83,56 @@ def article_list():
 def logout():
     logout_user()
     return redirect('/lab8/')
+
+
+@lab8.route('/lab8/create', methods=['GET', 'POST'])
+@login_required
+def create_article():
+    if request.method == 'GET':
+        return render_template('lab8/create_article.html')
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    if title == '' or article_text == '':
+        return render_template('lab8/create_article.html', error='Заполните все поля')
+
+    new_article = articles(title=title, article_text=article_text, login_id=current_user.id)
+    db.session.add(new_article)
+    db.session.commit()
+    return redirect('/lab8/articles/')
+
+
+@lab8.route('/lab8/edit/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def edit_article(article_id):
+    article = articles.query.get_or_404(article_id)
+
+    if article.login_id != current_user.id:
+        return redirect('/lab8/articles/')
+
+    if request.method == 'GET':
+        return render_template('lab8/edit_article.html', article=article)
+
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+
+    if title == '' or article_text == '':
+        return render_template('lab8/edit_article.html', article=article, error='Заполните все поля')
+
+    article.title = title
+    article.article_text = article_text
+    db.session.commit()
+    return redirect('/lab8/articles/')
+
+
+@lab8.route('/lab8/delete/<int:article_id>', methods=['POST'])
+@login_required
+def delete_article(article_id):
+    article = articles.query.get_or_404(article_id)
+
+    if article.login_id == current_user.id:
+        db.session.delete(article)
+        db.session.commit()
+    return redirect('/lab8/articles/')
 
