9a45ad9aba1499f53c919fb1ba5d8a08d2dbd89d
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-12-18 02:18:53 +0700

    Дополнительное задание

--- lab9.py
+++ lab9.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, session
 
 lab9 = Blueprint('lab9', __name__)
 
 
 @lab9.route('/lab9/', methods=['GET', 'POST'])
 def lab():
+    if 'message' in session:
+        message = session['message']
+        image = session['image']
+        return render_template('lab9/congratulation.html', message=message, image=image)
     if request.method == 'POST':
         name = request.form['name']
         return redirect(url_for('lab9.age', name=name))
@@ -66,26 +70,49 @@ def congratulation():
     message = ""
     image = ""
 
-    if age < 18: 
+    if age < 18:
         message = f"Поздравляю тебя, {name}, желаю, чтобы ты быстро {'вырос' if gender == 'male' else 'выросла'}, был{' умным' if gender == 'male' else 'а умной'}. Вот тебе подарок — "
 
         if preference == 'вкусное':
-            message += "мешочек конфет."
-            image = 'candies.jpg'
-        else:
-            message += "красивую игрушку."
-            image = 'toy.jpg'
+            if sub_preference == 'шоколадное':
+                message += "коробка конфет."
+                image = url_for('static', filename='candy.jpg')  
+            else: 
+                message += "вкусный торт."
+                image = url_for('static', filename='cake.jpg')  
+        else:  
+            if sub_preference == 'декоративное':
+                message += "елочные игрушки."
+                image = url_for('static', filename='decoration.jpg') 
+            else: 
+                message += "теплый свитер."
+                image = url_for('static', filename='sweater.jpg')
 
-    else: 
+    else:
         message = f"Поздравляю, {name}! Желаю тебе успехов и новых достижений. Надеюсь, что ты будешь наслаждаться жизнью. Вот твой подарок — "
 
         if preference == 'вкусное':
-            message += "сладкий торт."
-            image = 'cake.jpg'
-        else:
-            message += "практичный подарок."
-            image = 'practical_gift.jpg'
+            if sub_preference == 'шоколадное':
+                message += "коробка конфет."
+                image = 'lab9/candy.jpg'
+            else: 
+                message += "вкусный торт."
+                image = 'lab9/cake.jpg'
+        else:  
+            if sub_preference == 'декоративное':
+                message += "елочные игрушки."
+                image = 'lab9/decoration.jpg'
+            else: 
+                message += "теплый свитер."
+                image = 'lab9/sweater.jpg'
 
+    session['message'] = message
+    session['image'] = image
     return render_template('lab9/congratulation.html', message=message, image=image)
 
+
+@lab9.route('/lab9/reset/')
+def reset():
+    session.clear()
+    return redirect(url_for('lab9.lab'))
 
