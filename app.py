ea9d82d2ba04f255f715a21469dacf411812e6dc
Оноприенко Анжелика <anjik14.01@gmail.com>
2024-12-23 23:41:24 +0700

    Вход и регистрация пользователей

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
 from lab9 import lab9
+from rgz import rgz
 
 app = Flask(__name__)
 
@@ -53,6 +54,7 @@ app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
 app.register_blueprint(lab9)
+app.register_blueprint(rgz)
 
 
 @app.route("/")
@@ -98,8 +100,11 @@ def index():
                     <li>
                         <a href="/lab8/">Восьмая лабораторная</a>
                     </li>
-                     <li>
+                    <li>
                         <a href="/lab9/">Девятая лабораторная</a>
+                    </li>
+                    <li>
+                        <a href="/rgz/">Расчетно-графическое задание</a>
                     </li>
                 </ol>
             </div>
---
+++ rgz.py
@@ -0,0 +1,145 @@
+from flask import Blueprint, redirect, render_template, request
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import db
+from db.models import users, books
+from flask_login import login_user, login_required, current_user, logout_user
+
+rgz = Blueprint('rgz', __name__)
+
+@rgz.route('/rgz/')
+def lab():
+    return render_template('rgz/rgz.html')
+
+
+@rgz.route('/rgz/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect('/rgz/')
+    
+    if request.method == 'POST':
+        login_form = request.form['login']
+        password_form = request.form['password']
+
+        if not login_form or not password_form:
+            return render_template('rgz/register.html', error='Заполните все поля')
+
+        login_exists = users.query.filter_by(login=login_form).first()
+        if login_exists:
+            return render_template('rgz/register.html', error='Такой пользователь уже существует')
+
+        password_hash = generate_password_hash(password_form)
+        new_user = users(login=login_form, password=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect('/rgz/login')
+
+    return render_template('rgz/register.html')
+
+
+@rgz.route('/rgz/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/rgz/')
+    
+    if request.method == 'POST':
+        login_form = request.form['login']
+        password_form = request.form['password']
+
+        user = users.query.filter_by(login=login_form).first()
+        if user and check_password_hash(user.password, password_form):
+            login_user(user)
+            return redirect('/rgz/')
+
+        return render_template('rgz/login.html', error='Неверный логин или пароль')
+
+    return render_template('rgz/login.html')
+
+
+@rgz.route('/rgz/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/rgz/')
+
+
+@rgz.route('/rgz/books/', methods=['GET'])
+@login_required
+def book_list():
+    page = request.args.get('page', 1, type=int)
+    title_filter = request.args.get('title')
+    author_filter = request.args.get('author')
+    pages_min = request.args.get('pages_min', type=int)
+    pages_max = request.args.get('pages_max', type=int)
+    publisher_filter = request.args.get('publisher')
+
+    query = Book.query
+
+    if title_filter:
+        query = query.filter(Book.title.ilike(f'%{title_filter}%'))
+    if author_filter:
+        query = query.filter(Book.author.ilike(f'%{author_filter}%'))
+    if pages_min is not None:
+        query = query.filter(Book.pages >= pages_min)
+    if pages_max is not None:
+        query = query.filter(Book.pages <= pages_max)
+    if publisher_filter:
+        query = query.filter(Book.publisher.ilike(f'%{publisher_filter}%'))
+
+    books = query.paginate(page, per_page=20)
+
+    return render_template('rgz/book_list.html', books=books)
+
+
+@rgz.route('/rgz/books/add', methods=['GET', 'POST'])
+@login_required
+def add_book():
+    if current_user.login != 'Admin':
+        return redirect('/rgz/books/')
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        pages = request.form['pages']
+        publisher = request.form['publisher']
+        cover_image = request.form['cover_image']
+
+        new_book = Book(title=title, author=author, pages=pages, publisher=publisher, cover_image=cover_image)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect('/rgz/books/')
+
+    return render_template('rgz/add_book.html')
+
+
+@rgz.route('/rgz/books/edit/<int:book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    if current_user.login != 'Admin':
+        return redirect('/rgz/books/')
+
+    book = Book.query.get_or_404(book_id)
+
+    if request.method == 'POST':
+        book.title = request.form['title']
+        book.author = request.form['author']
+        book.pages = request.form['pages']
+        book.publisher = request.form['publisher']
+        book.cover_image = request.form['cover_image']
+
+        db.session.commit()
+        return redirect('/rgz/books/')
+
+    return render_template('rgz/edit_book.html', book=book)
+
+
+@rgz.route('/rgz/books/delete/<int:book_id>', methods=['POST'])
+@login_required
+def delete_book(book_id):
+    if current_user.login != 'Admin':
+        return redirect('/rgz/books/')
+
+    book = Book.query.get_or_404(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect('/rgz/books/')
